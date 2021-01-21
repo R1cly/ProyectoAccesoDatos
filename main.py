8f55b3a4b5c41c50d40ce4292cb04b61fa1717ee
Ricardo Sevilla Climent <57192844+R1cly@users.noreply.github.com>
2021-01-21 18:28:48 +0100

    He avanzado algo.

--- main.py
+++ main.py
@@ -1,42 +1,73 @@
 import tkinter as tk
+import mysql.connector
+
 
 class botonesMenu():
-    def __init__(self, mainHeader,inputX,inputY,comandoBoton):
-
+    def __init__(self, mainHeader, inputX, inputY, comandoBoton,texto):
         self.mainHeader = mainHeader
 
-        self.button11 = tk.Button(self.mainHeader,width="15",height="5",
-                                  command=comandoBoton)
-        self.button11.pack()
+        self.button11 = tk.Button(self.mainHeader, width="15", height="5",
+                                  command=comandoBoton,bg="blue",text=texto)
+        self.button11.pack(padx="10", pady="10")
 
-        self.button11.place(x=inputX,y=inputY)
 
 class ventanaEstandar():
+
     def __init__(self, master):
-        #Main header para todas las ventanas
+        # Main header para todas las ventanas
         self.master = master
-        self.mainHeader = tk.Frame(self.master,bg="#7bed9f",height="1100",width="135",
-                                   highlightbackground="black",highlightthickness=1.5)
 
+        self.mainWindow = tk.Frame(self.master, bg="#494E55", height="1080", width="1980")
+        self.mainHeader = tk.Frame(self.mainWindow, bg="#8CBE8D", height="1100", width="135",
+                                   highlightbackground="black", highlightthickness=1.5)
 
-        boton012 = botonesMenu(mainHeader=self.mainHeader,inputX=10,inputY=0,comandoBoton="null")
-        boton011 = botonesMenu(mainHeader=self.mainHeader, inputX=10, inputY=110, comandoBoton="null")
-        boton013 = botonesMenu(mainHeader=self.mainHeader, inputX=10, inputY=200, comandoBoton="null")
+        self.databaseHeader = tk.Frame(self.mainWindow, width="250",height="1080")
+
+        self.databaseHeader.pack_propagate(False)
+        self.databaseHeader.pack(side="right")
+
+        self.botonN = botonesMenu(mainHeader=self.mainHeader, inputX=10, inputY=200, comandoBoton=self.crearTablas,texto="Hola")
+
+        self.mainWindow.pack_propagate(False)
+        self.mainWindow.pack()
 
         self.mainHeader.pack_propagate(False)
         self.mainHeader.pack(side="left")
 
+    def crearTablas(self):
+
+        myCursor.execute("SHOW DATABASES")
+
+        self.databaseText = tk.Label(self.databaseHeader,text="Database")
+        self.databaseText.pack(pady=10,padx=10)
+        self.databaseText.pack_propagate(False)
+
+        for x in myCursor:
+            print(x)
+            boton = botonesMenu(mainHeader=self.databaseHeader, inputX=10, inputY=200, comandoBoton=main,texto=x)
+
+
+def helloFriend():
+    print("Hello")
+
+
 def main():
     root = tk.Tk()
-    root.geometry("1980x1080")
+    root.geometry("1600x900")
     root.title("Administracion de centros educativos")
+
+
     app = ventanaEstandar(root)
+    #ventanaEstandar.crearTablas(app)
 
     root.mainloop()
 
-def hola():
-    print("Hola")
-
 
 if __name__ == '__main__':
+    myDatabase = mysql.connector.connect(host="localhost",
+                                         username="root",
+                                         password="a")
+
+    myCursor = myDatabase.cursor()
+
     main()
